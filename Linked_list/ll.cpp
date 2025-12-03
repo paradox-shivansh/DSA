785605f0d3fb9ff151f8d0662858ddb752edeebb
paradox-shivansh <theaverageguy19@gmail.com>
2025-12-04 05:11:03 +0530

    linked list

---
+++ Linked_list/ll.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+
+};
+
+void input(Node* &ll , int data){
+    
+}
